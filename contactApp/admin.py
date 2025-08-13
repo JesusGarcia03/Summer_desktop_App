f8dbc4e73b966a8c5e6f7bf03ba6b0227a73b4f4
Jesus Garcia <jesus.garcia56@utrgv.edu>
2025-08-12 19:36:39 -0500

    changes as of 8-12-25

--- contactApp/admin.py
+++ contactApp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Record
+from .models import Record, Profile, Post, Comment
 
 # Register your models here.
 admin.site.register(Record)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
--- contactApp/models.py
+++ contactApp/models.py
@@ -1,14 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Record(models.Model):
 	create_at = models.DateTimeField(auto_now_add=True)
 	first_name = models.CharField(max_length=20)
 	last_name = models.CharField(max_length=20)
+	#bio = models.CharField(max_length=100)
 	#email = models.EmailField(maxlength=30)
 	email = models.CharField(max_length=30)
 	phone = models.CharField(max_length=10)
 
 
-	def _str_(self):
-		return(f"{slef.first_name}{self.last_name}")
+	def __str__(self):
+		return(f"{self.first_name}{self.last_name}")
+	
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	phone = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.user.username
+	
+class Post(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	
+	def __str__(self):
+		return f"{self.author.username} posted at {self.created_at}"
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	text = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.author.username} commented on {self.post.id}"
+	
